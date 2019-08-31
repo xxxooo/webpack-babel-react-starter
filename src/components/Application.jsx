7a86cc06c2da242169d1887f150c584bdc6f4550
TimChen <timchen@m800.com>
2019-08-31 13:18:41 +0800

    Style and image loader
    - Add sass loader
    - Add url-loader & file-loader
    - Refactor webpack config
    - Change example component style

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './Application.scss';
+import reactIcon from '../images/React.svg';
 
 function Application() {
   return (
-    <div>Hello React!</div>
+    <div className="app-container">
+      <h1>Hello React!</h1>
+      <img src={reactIcon} alt="react-icon" />
+    </div>
   );
 }
 
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,37 +1,94 @@
 import HtmlWebPackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export default {
-  module: {
-    rules: [
+export default (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const isProduction = argv.mode === 'production';
+
+  const scriptLoader = {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+    },
+  };
+
+  const styleLoader = {
+    test: /\.(sa|sc|c)ss$/,
+    use: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          publicPath: '../',
+          hmr: isDevelopment,
         },
       },
+      'css-loader',
+      'sass-loader',
+    ],
+  };
+
+  const urlLoader = {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    use: [
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          outputPath: 'images',
+          name: '[name]-[contenthash:6].[ext]',
+        },
       },
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new BundleAnalyzerPlugin({
+  };
+
+  const templateLoader = {
+    test: /\.html$/,
+    use: [
+      {
+        loader: 'html-loader',
+      },
+    ],
+  };
+
+  const config = {
+    output: {
+      filename: '[name]-[contenthash:6].js',
+    },
+    devtool: isDevelopment && 'source-map',
+    module: {
+      rules: [
+        scriptLoader,
+        styleLoader,
+        urlLoader,
+        templateLoader,
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json'],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name]-[contenthash:6].css',
+      }),
+    ],
+  };
+
+  if (isProduction) {
+    // Clean dist folder
+    config.plugins.unshift(new CleanWebpackPlugin());
+    // Add bundle analyzer
+    config.plugins.push(new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       reportFilename: './bundleAnalyzer/report.html',
-    }),
-  ],
+    }));
+  }
+
+  return config;
 };
